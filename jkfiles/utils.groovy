c15f94938fd0deece9df4d57d4a7df1b46c8b83d
devOps <love-qcf@163.com>
2018-01-30 16:58:52 +0800

    changes

--- jkfiles/utils.groovy
+++ jkfiles/utils.groovy
@@ -1,11 +1,9 @@
 echo "utils begin..."
-
+def cfg=readProperties file:"jkfiles/cfg.properties"
+def a = cfg['name']
+echo "${a}"
 def getCfg() {
-    def cfg=readProperties file:"jkfiles/cfg.properties"
-    def a = cfg['name']
-    echo "${a}"
     cfg;
 }
-
 echo "utils end.."
 this
